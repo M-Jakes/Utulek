563b5cfbcaaafafc2fa4fbf97aaf2831eabe1785
M-Jakes <jakesmatyas@gmail.com>
2026-02-27 11:48:07 +0100

    images correction pt.2

--- db_config.php
+++ db_config.php
@@ -1,12 +1,11 @@
 <?php
-$host = "sql102.infinityfree.com";
-$db_user = "if0_41258267"; // Výchozí v XAMPP
-$db_pass = "Komarno39";     // Výchozí v XAMPP je prázdné
-$db_name = "if0_41258267_XXX";
+$host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "utulek";
 
 $conn = new mysqli($host, $db_user, $db_pass, $db_name);
 
-// Kontrola připojení
 if ($conn->connect_error) {
     die("Připojení selhalo: " . $conn->connect_error);
 }
